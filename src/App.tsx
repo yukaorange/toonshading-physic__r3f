4246836945a04d16b582b82b8c578cbb9246cd1e
yukaorange <takaoka.webdesigner@gmail.com>
2024-06-30 06:41:03 +0900

    chore: Add lazy loading for images and update npm dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
-
 import { Experience } from '@/components/Experience'
 import { Sns } from '@/components/Sns'
 import { MenuButton } from '@/components/MenuButton'
 import { Loader } from '@react-three/drei'
 import { useControls, Leva } from 'leva'
 import { useRef } from 'react'
+import { Suspense } from 'react'
 
 const App = () => {
   return (
@@ -22,8 +22,10 @@ const App = () => {
           fov: 45,
         }}
       >
-        <Perf position="top-left" />
-        <Experience />
+        <Suspense fallback={null}>
+          <Perf position="top-left" />
+          <Experience />
+        </Suspense>
       </Canvas>
     </>
   )
